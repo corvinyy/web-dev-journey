e4a833de439389d6b3b2e3ae36082ef0588f773d
corvinyy <cviny072@gmail.com>
2025-07-24 22:51:05 -0300

    feat: add switch-case and ternary exercises

---
+++ 02-js-docs/02_operators_and_functions/switch-case-ternary_exercises.js
@@ -0,0 +1,23 @@
+// bank account options
+let option = 2;
+switch (option) {
+    case 1:
+        console.log("account balance");
+        break;
+    case 2:
+        console.log("credit card");
+        break;
+    case 3:
+        console.log("undue discounts");
+        break;
+    default:
+        console.log("invalid option");
+        break;
+}
+console.log("==========================================");
+
+// Verifying age 
+let age = 18;
+//age = 18 ? true : false;
+age >= 18 ? console.log("You are an adult.") : console.log("You are a minor.");
+console.log("==========================================");
