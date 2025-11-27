3b227fcb8818b9660e96c9d3d3bcde5784b02935
corvinyy <cviny072@gmail.com>
2025-11-27 00:24:08 -0300

    feat: basic array operations in JavaScript: accessing elements, adding with push, and deleting an element.

---
+++ 02-js-docs/03_arrays/script.js
@@ -0,0 +1,15 @@
+let name = "Lorena";
+
+let names = ["Maria", "João", "Pedro"]
+console.log(names); // Accessing the entire array
+console.log(names[2]); // Accessing the third element of the array
+
+
+//Adding elements to the array
+names.push("Ana"); // Adds "Ana" to the end of the array
+console.log(names);
+
+
+//Removing the last element from the array
+delete names[0] // Removes the first element (Maria) from the array
+console.log(names); 
